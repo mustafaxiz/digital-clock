fa45d99104767ed82b45cb005e3425e76d5ff50a
Mustafa Zaki <mustafaosts@gmail.com>
2025-11-15 21:14:17 +0400

    Add files via upload

---
+++ digital-clock.js
@@ -0,0 +1,17 @@
+function clockUpdate(){
+    const now = new Date();
+    let  hours = now.getHours();
+    const postMeridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2,0);
+    const minutes = now.getMinutes().toString().padStart(2,0);
+    const seconds = now.getSeconds().toString().padStart(2,0);
+    const timeString = `${hours}:${minutes}:${seconds} ${postMeridiem}`;
+    document.getElementById("digitalClock").textContent = timeString;
+}
+
+clockUpdate();
+setInterval(clockUpdate, 1000);
+
+
+
